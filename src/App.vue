83f6ea0977eb8332cfc954e7e435b41e20eeb7a3
LeoDu2017 <2516849822@qq.com>
2018-12-25 19:07:55 +0800

    全局搜索组件

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,23 @@
 export default {
   created () {
     // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    const logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
 
     console.log('app created and cache logs by setStorageSync')
   }
 }
 </script>
 
-<style>
+<style lang="less">
+  @import "./icon/iconfont.css";
 .container {
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  padding: 200rpx 0;
   box-sizing: border-box;
 }
 /* this rule will be remove */
