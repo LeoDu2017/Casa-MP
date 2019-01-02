ad35074748d67a98b92a77d997985decc8fbf414
LeoDu2017 <2516849822@qq.com>
2019-01-02 15:09:35 +0800

    if (!this.isSend) {                wx.showToast({                  title: '您还没有获取验证码，请获取验证码后再提交！',                  icon: 'none'                })                return false              }

--- src/pages/ucenter/sign/index.js
+++ src/pages/ucenter/sign/index.js
@@ -1,4 +1,4 @@
-import { isPhoneNo, formatNumber } from '@/utils/index'
+import { isPhoneNo, isEmail, formatNumber } from '@/utils/index'
 import store from '@/status/store'
 import UTitle from '@/components/units/u-title'
 import UInput from '@/components/units/u-input'
@@ -7,12 +7,16 @@ export default {
     return {
       timeState: '获取验证码',
       phone: null,
+      code: null,
       password: null,
+      password_02: null,
+      isSend: false,
       sex: 0,
       picked: '',
       currentTime: 60,
       hide01: true,
       hide02: true,
+      email: null,
     }
   },
   components: {
@@ -27,19 +31,17 @@ export default {
       this.password = val
     },
     confirmPassword (val) {
-      if (this.password !== val){
-        wx.showToast({
-          title: '两次输入的密码不相同！',
-          icon: 'none'
-        })
-      }
+      this.password_02 = val
+    },
+    setCode (val) {
+      this.code = val
     },
     setSex (e) {
       const {target:{value}} = e
       this.sex = value
     },
-    setEmail () {
-
+    setEmail (val) {
+      this.email = val
     },
     onGain () {
       const interval = setInterval(() => {
@@ -72,6 +74,7 @@ export default {
         },
         success: (res) => {
           if (res.data.status === 1) {
+            this.isSend = true
             this.onGain()
           } else if (res.data.status === 0) {
             wx.showToast({
@@ -85,6 +88,116 @@ export default {
     },
     onSee (state) {
       this[state] = !this[state]
+    },
+    onSubmit () {
+      if (!this.verification()) {
+        return
+      }
+      wx.request({
+        url: `${store.state.url}/wxapi/login/ajaxEnroll`,
+        method: 'POST',
+        data: {
+          'csr_mobile': this.phone,
+          'code': this.code,
+          'csr_password': this.password,
+          'csr_email': this.email,
+          'csr_sex': this.sex
+        },
+        header: {
+          'Accept': 'application/json'
+        },
+        success: (res) => {
+          if(res.data.status === 1){
+            wx.showToast({
+              title: '注册成功',
+              icon: 'none'
+            })
+            wx.reLaunch({
+              url: '/pages/ucenter/login/main'
+            })
+            return
+          } else if (res.data.status === -3) {
+            wx.showModal({
+              title: '提示',
+              content: '验证码错误',
+              showCancel: false,
+            })
+            return false;
+          }else if (res.data.status === -4) {
+            wx.showModal({
+              title: '提示',
+              content: '手机号已存在',
+              showCancel: false,
+            })
+            return false;
+          }else if (res.data.status === -7) {
+            wx.showModal({
+              title: '提示',
+              content: '手机号错误',
+              showCancel: false,
+            })
+            return false;
+          } else  {
+            wx.showModal({
+              title: '提示',
+              content: '注册失败，请联系客服解决',
+              showCancel: false
+            })
+            return false;
+          }
+        }
+      });
+    },
+    verification () {
+      if (!isPhoneNo(this.phone)) {
+        wx.showToast({
+          title: '请输入正确的手机号码！',
+          icon: 'none'
+        })
+        return false
+      }
+      if (!this.isSend) {
+        wx.showToast({
+          title: '您还没有获取验证码，请获取验证码后再提交！',
+          icon: 'none'
+        })
+        return false
+      }
+      if (!this.code) {
+        wx.showToast({
+          title: '请输入您收到的验证码！',
+          icon: 'none'
+        })
+        return false
+      }
+      if (!this.password) {
+        wx.showToast({
+          title: '请设置您的登录密码！',
+          icon: 'none'
+        })
+        return false
+      }
+      if (!this.password_02) {
+        wx.showToast({
+          title: '请确认您的登录密码！',
+          icon: 'none'
+        })
+        return false
+      }
+      if (this.password !== this.password_02) {
+        wx.showToast({
+          title: '您两次输入的密码不相同，请检查后提交！',
+          icon: 'none'
+        })
+        return false
+      }
+      if (this.email && !isEmail(this.email)) {
+        wx.showToast({
+          title: '请输入正确的邮箱地址，或者留空！',
+          icon: 'none'
+        })
+        return false
+      }
     }
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,10 @@ export function isPhoneNo (phone) {
   var pattern = /^1[34578]\d{9}$/
   return pattern.test(phone)
 }
+export function isEmail (email) {
+  var pattern = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/
+  return pattern.test(email)
+}
 export function formatTime (date) {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -23,5 +27,6 @@ export function formatTime (date) {
 export default {
   formatNumber,
   formatTime,
-  isPhoneNo
+  isPhoneNo,
+  isEmail
 }
