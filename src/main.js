74b52e91ccc75ab4b7eb475b63cd56f613fedc65
LeoDu2017 <2516849822@qq.com>
2019-01-07 09:44:02 +0800

    使用config字段 取缔app.json

--- src/main.js
+++ src/main.js
@@ -6,3 +6,25 @@ App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
+
+export default {
+  config: {
+    pages: [
+      'pages/web/main',
+      'pages/logs/main',
+      'pages/index/main',
+      'pages/search/main',
+      'pages/counter/main',
+      'pages/ucenter/sign/main',
+      'pages/ucenter/login/main',
+      'pages/ucenter/forget/main',
+      'pages/product/detail/main',
+      '^pages/ucenter/account/main'],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: '有容-意大利之家',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
