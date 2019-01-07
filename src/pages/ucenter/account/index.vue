a4827126be935694c0c1d70993266021d92f4b59
LeoDu2017 <2516849822@qq.com>
2019-01-07 20:05:17 +0800

    个人中心页面

--- src/pages/ucenter/account/index.vue
+++ src/pages/ucenter/account/index.vue
@@ -1,47 +1,84 @@
 <template>
-  <canvas canvas-id="canvas" :style="{width: width + 'px', height: height+'px'}"></canvas>
+  <div class="account">
+    <div class="profile_box">
+      <canvas canvas-id="wave" :style="{height:height + 'px'}"></canvas>
+      <div class="profile_photo" :style="{top:height - 63 + 'px'}">
+        <i class="iconfont icon-zhanghu"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        width: 0,
         height: 0
       }
     },
-    canvasOptions: {
-      canvasId: 'canvas'
+    onLoad () {
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        fontFamily: '微软雅黑',
+        backgroundColor: '#212224',
+        animation: {
+          duration: 100,
+          timingFunc: 'easeIn'
+        }
+      })
     },
-    renderCanvas (h) {
-      let device = wx.getSystemInfoSync()
-      this.width = device.windowWidth
-      this.height = device.windowHeight
-      return h('view', [
-        h('image', {
-          props: {
-            src: 'https://pic.u51.com/sfs-gateway/api/v1/download/5f7dac8228354008ae6f69f67c1c0fa410d6'
-          },
-          style: {
-            left: 10,
-            top: 10,
-            width: 100,
-            height: 100,
-            fill: '#000',
-            fontSize: 18
-          }
-        }),
-        h('text', {
-          style: {
-            left: 120,
-            top: 10,
-            fill: '#000',
-            fontSize: 18,
-            width: 150,
-            ellipse: true
-          }
-        }, 'hello mpvue!')
-      ])
+    onReady () {
+      this.drawWave()
+    },
+    methods: {
+      drawWave () {
+        const ctx = wx.createCanvasContext('wave')
+        const width = wx.getSystemInfoSync().windowWidth
+        this.height = width / 2
+        ctx.beginPath()
+        ctx.moveTo(0, 0)
+        ctx.lineTo(0, this.height - 30)
+        ctx.bezierCurveTo(width / 4, this.height + 20, width * 3 / 4, this.height - 70, width, this.height - 30)
+        ctx.lineTo(width, 0)
+        ctx.fillStyle = '#212224'
+        ctx.closePath()
+        ctx.fill()
+        ctx.draw()
+      }
     }
   }
 </script>
+<style scoped lang="less">
+  .account{
+    width: 100vw;
+    height: 100vh;
+
+    .profile_box{
+      position: relative;
+      canvas {
+        width: 100%;
+      }
+      .profile_photo{
+        width: 66px;
+        height: 66px;
+        line-height: 66px;
+        background-color: #DEDEDE;
+        border:4px solid #fff;
+        border-radius: 50%;
+        position: absolute;
+        cursor: pointer;
+        box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.1);
+        left: 50%;
+        transform: translateX(-50%);
+        color: #999;
+        text-align: center;
+        .icon-zhanghu{
+          font-size: 30px;
+        }
+      }
+
+    }
+
+  }
+
+</style>
--- src/pages/ucenter/account/main.js
+++ src/pages/ucenter/account/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
-import App from './index.vue'
+import App from './index'
 
 const app = new Vue(App)
 app.$mount()
+
+export default {
+  config: {
+    navigationBarTitleText: 'scroll view',
+    disableScroll: true
+  }
+}
