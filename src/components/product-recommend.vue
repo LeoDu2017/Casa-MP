85331580fc628dfba54f8da3d653fa8765cca29d
LeoDu2017 <2516849822@qq.com>
2019-02-18 13:50:56 +0800

    首页底部 数据计算

---
+++ src/components/product-recommend.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="common-block">
+    <common-title :title="title" :more="more" :height="32"></common-title>
+    <ul class="product-wrap">
+      <product-window v-for="product in products" :product="product" :key="product.id" :min="true">
+      </product-window>
+    </ul>
+  </div>
+</template>
+<script>
+  import CommonTitle from './units/c-title.vue'
+  import ProductWindow from './units/model-one.vue'
+  export default {
+    props: ['title', 'products', 'more'],
+    components: {
+      CommonTitle,
+      ProductWindow
+    }
+  }
+</script>
+<style scoped lang="less">
+  .common-block{
+    padding: 15px 15px 5px;
+    background: #fff;
+    .product-wrap{
+      display:flex;
+      padding-top:15px;
+      flex-wrap:wrap;
+      justify-content: space-between;
+      overflow:hidden;
+    }
+  }
+</style>
